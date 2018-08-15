e10a18a382579371949bc167d93f4587eebc96bc
Andrew Keesler <ankeesler1@gmail.com>
2018-08-14 21:25:29 -0400

    Get rid of anvm, integration tests, and test script. Going a different direction.

--- test/anvm/integration/fixtures/regen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-bazel build //src/assembler:anasm
-./bazel-bin/src/assembler/anasm -o test/anvm/integration/fixtures/basic.out test/assembler/integration/fixtures/basic.asm
--- test/asm/integration/fixtures/bad.asm
+++
@@ -1,6 +0,0 @@
-tuna:
-	STORE %r0 @%sp
-	LOAD %sp %r0
-	LOAD 4 %r1
-	ADD
-	LOAD %r0 %sp 
