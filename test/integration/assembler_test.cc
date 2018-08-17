2a12ec410ccb02d36c17a48b2781fdf277b11019
Andrew Keesler <akeesler@pivotal.io>
2018-08-17 13:13:18 -0400

    OK so this is how we run async test functions in C++?

--- test/integration/assembler_test.cc
+++ test/integration/assembler_test.cc
@@ -1,16 +1,25 @@
 #include <fstream>
 #include <sstream>
+#include <future>
+#include <thread>
+#include <chrono>
 
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "src/cpu.h"
 #include "src/asm/assembler.h"
 #include "src/util/log.h"
 #include "src/util/error.h"
+#include "src/util/program_reader.h"
 
 class AssemblerTest : public testing::Test {
     protected:
         Assembler asm_;
 };
+
+static void RunProgram(const Program& p, std::promise<Word> *promise) {
+    promise->set_value(5);
+}
 
 TEST_F(AssemblerTest, Bad) {
     std::ifstream ifs("./test/integration/fixtures/bad.asm");
@@ -27,8 +36,26 @@ TEST_F(AssemblerTest, Good) {
     std::ifstream ifs("./test/integration/fixtures/good.asm");
     ASSERT_TRUE(ifs);
 
-    std::ostringstream oss;
+    std::stringstream ss;
 
-    Error error = asm_.Run(new StdoutLog(), ifs, oss);
-    ASSERT_FALSE(error);
+    Error error1 = asm_.Run(new StdoutLog(), ifs, ss);
+    ASSERT_FALSE(error1);
+
+    Program p(
+            ILOAD, 1, 0,
+            ILOAD, 2, 1,
+            IADD,
+            IEXIT
+           );
+    std::promise<Word> promise;
+    std::thread thread(RunProgram, std::ref(p), &promise);
+    std::future<Word> fut = promise.get_future();
+    std::future_status fut_status = fut.wait_for(std::chrono::seconds(3));
+    if (fut_status == std::future_status::timeout) {
+        std::cout << "timeout!" << std::endl;
+        ASSERT_TRUE(false);
+    } else {
+        std::cout << "it finished!" << std::endl;
+        ASSERT_TRUE(true);
+    }
 }
